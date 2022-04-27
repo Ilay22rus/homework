8177fc9b6973559682402f266d9695e6c8a7b8cb
Ilay <ilay22rus@mail.ru>
2022-04-27 20:29:45 +0700

    hhh

---
+++ task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if (number < 999999)
+{
+Console.WriteLine(number % 1000 / 100);
+}
+if (number < 999)
+{
+Console.WriteLine(number % 10);
+}
+if (number < 99)
+{
+Console.WriteLine("Третьей цыфры нет");
+}
---
+++ task_2/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if(number == 1)
+{
+Console.WriteLine("понедельник");
+}
+if(number == 2)
+{
+Console.WriteLine("Вторник");
+}
+if(number == 3)
+{
+Console.WriteLine("среда");
+}
+if(number == 4)
+{
+Console.WriteLine("Четверг");
+}
+if(number == 5)
+{
+Console.WriteLine("пятница");
+}
+if(number == 6)
+{
+Console.Write("Суббота");
+}
+if(number == 7)
+{
+Console.Write("Воскресенье");
+}
+if(number == 6 & number == 7)
+{
+Console.WriteLine("выходной");
+}
